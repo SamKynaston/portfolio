dad61d9e1d9c188b0411aa5f04531941b7e05d16
Sam <samuelkynno@gmail.com>
2023-08-18 00:33:02 +0100

    deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({config: {applyBaseStyles:false}}), react()]
+  integrations: [tailwind({config: {applyBaseStyles:false}}), react()],
+  site:'https://samkynaston.github.io/',
+  base:'/portfolio'
 });
