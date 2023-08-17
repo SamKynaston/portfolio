998a6b12883a41e0e9496be995bb625c7ee7da31
Sam <samuelkynno@gmail.com>
2023-08-17 22:35:50 +0100

    Added basic navigation

---
+++ src/components/navigation.tsx
@@ -0,0 +1,24 @@
+import "../styles/navigation.css";
+import {createRef, Component} from "react";
+
+type Props = {};
+type State = {};
+
+class Navigation extends Component<Props, State> {
+    state: State = {}
+    
+    render() {
+        return (
+            <nav>
+                <span className="nav-left">
+                    <a href="./">Kynno</a>
+                </span>
+                <span className="nav-right">
+                    <a href="./portfolio">Portfolio</a>
+                </span>
+            </nav>
+        )
+    }
+}
+
+export default Navigation
