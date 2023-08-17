76727df4d65335248f125c96cb7738e47a988ef3
Sam <samuelkynno@gmail.com>
2023-08-17 22:53:11 +0100

    colors test 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"cozy": {
+					300: '#A77979',
+					500: '#704F4F',
+					700: '#553939',
+					900: '#472D2D'
+				},
+			},
+		},
 	},
 	plugins: [],
 }
