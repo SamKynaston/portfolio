d863812908baea9b811608c76bd29df8104d6ba2
Sam <samuelkynno@gmail.com>
2023-08-18 00:28:02 +0100

    Added in basic portfolio page w/ flexbox

---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+import "../styles/navigation.css";
+import {createRef, Component} from "react";
+
+type Props = {};
+type State = {};
+
+class Footer extends Component<Props, State> {
+    state: State = {}
+    
+    render() {
+        return (
+            <footer>
+                
+            </footer>
+        )
+    }
+}
+
+export default Footer
