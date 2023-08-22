99f88da703f2c5c81179cfc9dade9322e1f786f2
Sam <samuelkynno@gmail.com>
2023-08-22 18:31:13 +0100

    Titles & Framework for portfolio page

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -14,8 +14,14 @@ const PortfolioItem: React.FC<Props> = ({ name, languages, frameworks, links }) 
           <box className="secondary">
               <h1 id="title">{name}</h1>
 
+              <h2 id="subtitle">Language(s) used</h2>
               <section className="centered">
                   {languages.map(language => {return (<TagItem name={language}/>)})}
+              </section>
+
+            <h2 id="subtitle">Framework(s) used</h2>
+              <section className="centered">
+                  {frameworks.map(framework => {return (<TagItem name={framework}/>)})}
               </section>
 
               <hr id="main" />
