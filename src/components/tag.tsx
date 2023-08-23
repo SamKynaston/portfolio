9ecd00a743537b718beefb758f8398bfd93f90a3
Sam <samuelkynno@gmail.com>
2023-08-23 10:44:28 +0100

    Added in display names for tags

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,13 +1,17 @@
 import type React from "react";
 import "../styles/tags.css"
 
+let displayNames = {
+  "C-sharp":"C#"
+}
+
 interface Props {
     name: string;
 }
 
 const TagItem: React.FC<Props> = ({ name }) => {
   return (
-      <p className="tag mx-1" id={name.toLowerCase()}> {name} </p>
+      <p className="tag mx-1" id={name.toLowerCase()}> {displayNames[name] || name} </p>
   );
 };
 
